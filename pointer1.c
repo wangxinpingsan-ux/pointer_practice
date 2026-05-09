b9f6dcf0c1323449d3df346c9e1cdcbd010ba853
wangxinpingsan-ux <wangxinpingsan@gmail.com>
2026-05-10 00:06:07 +0800

    finish

--- pointer1.c
+++ pointer1.c
@@ -1,1 +1,23 @@
 #include <stdio.h>
+
+int main (void){
+
+
+  int a = 5;
+  int *aptr = & a;
+
+
+  printf("a value = %d\n *aptr value = %d\n",a ,*aptr);//5
+  printf("a address = %p\n aptr address = %p\n",&a ,aptr);//address
+  printf("*&aptr = %p\n &*aptr = %p\n",*&aptr ,&*aptr);//address
+
+
+
+
+
+
+
+
+
+    return 0 ;
+}
