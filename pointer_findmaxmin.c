0f5301355e58aaf0c8215cc7d42d80483397ef28
wangxinpingsan-ux <wangxinpingsan@gmail.com>
2026-05-15 00:17:09 +0800

    1

---
+++ pointer_findmaxmin.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#define size 5
+
+void find_maxmin (int array[],int *max ,int *min){
+
+
+
+}
+
+
+
+
+
+
+int main (void){
+    
+    int number[]={35, 12, 89, 4, 56};
+    int max_value=0,min_value=0;
+
+    find_maxmin (number,number[0],number[0]);
+
+  
+
+
+   printf("max is %d",max_value);
+   printf("min is %d",min_value);
+
+    return 0;
+}
