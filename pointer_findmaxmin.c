f336011a1822538a8bd7c5f95198189229ea6d54
wangxinpingsan-ux <wangxinpingsan@gmail.com>
2026-05-16 11:20:58 +0800

    first commit

--- pointer_findmaxmin.c
+++ pointer_findmaxmin.c
@@ -1,29 +1,36 @@
 #include <stdio.h>
 #define size 5
 
-void find_maxmin (int array[],int *max ,int *min){
-
-
-
+void find_maxmin(int array[], int *max, int *min)
+{
+    *max = array[0];
+    *min = array[0];
+    for (int i = 1; i < size; i++)
+    {
+        if (*max < array[i])
+        {
+            *max = array[i];
+        }
+    }
+    for (int j = 1; j < size; j++)
+    {
+        if (*min > array[j])
+        {
+            *min = array[j];
+        }
+    }
 }
 
+int main(void)
+{
 
+    int number[] = {35, 12, 89, 4, 56};
+    int max_value = 0, min_value = 0;
 
+    find_maxmin(number, &max_value, &min_value);
 
-
-
-int main (void){
-    
-    int number[]={35, 12, 89, 4, 56};
-    int max_value=0,min_value=0;
-
-    find_maxmin (number,number[0],number[0]);
-
-  
-
-
-   printf("max is %d",max_value);
-   printf("min is %d",min_value);
+    printf("max is %d\n", max_value);
+    printf("min is %d", min_value);
 
     return 0;
 }
